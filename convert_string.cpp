0bcce2c83247c78c8e41fedc44396cf6007223ce
Chuck <chuck@Debian.homenet.telecomitalia.it>
2015-01-24 22:56:01 +0100

    Aggiornamento

--- convert_string.cpp
+++ convert_string.cpp
@@ -11,22 +11,29 @@ void maiuscolo(string& s)
 {
 	transform(s.begin(), s.end(), s.begin(),(int(*)(int)) toupper);
 }
+string scel(int scelta, string& s)
+{
+	if(scelta!=1 && scelta!=2)
+	{
+		cout<<"Scelta non valida\n";
+		return "riprova";
+	}
+	if(scelta==1)
+		maiuscolo(s);
+	if(scelta==2)
+		minuscolo(s);
+	return s;
+}
 int main()
 {
-string s;
-int scelta;
-cout<<"Inserisci la stringa che vuoi convertire\n";
-cin>>s;
-cout<<"Come convertire la stringa:\n1)Per convertire la stringa in maiuscolo digita 1\n2)Per convertire la stringa in minuscolo digita 2\n";
-cin>>scelta;
-if(scelta!=1 && scelta!=2)
-{
-	cout<<"Scelta non valida\n";
-	return 0;
-}
-if(scelta==1)
-	maiuscolo(s);
-if(scelta==2)
-	minuscolo(s);
-cout<<"La tua stinga è diventata:\n"<<s<<endl;
+	string s;
+	int scelta;
+	cout<<"Inserisci la stringa che vuoi convertire\n";
+	cin>>s;
+	cout<<"Come convertire la stringa:\n1)Per convertire la stringa in maiuscolo digita 1\n2)Per convertire la stringa in minuscolo digita 2\n";
+	cin>>scelta;
+	s=scel(scelta, s);
+	if(s=="riprova")
+		return 0;
+	cout<<"La tua stringa è diventata: "<<s<<endl;
 }
